8fea24f35b9805b811eaa60b6fc0bdeaa0fce037
phelixdusengimana <phelixdusengimana@gmail.com>
2023-04-10 14:30:29 +0200

    feat: login and register done

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#3C4DE9",
+        "secondary": "#6587FF",
+        "tertiary": "#3e3e3e",
+      },
+      fontFamily:{
+        "sans": ["Poppins", "sans-serif"],
+      },
+      boxShadow:{
+        "formCard": "0px 0px 4px rgba(0, 0, 0, 0.25)"
+      }
+    },
   },
   plugins: [],
 }
